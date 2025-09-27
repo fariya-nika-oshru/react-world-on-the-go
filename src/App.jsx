4ff7115a9daba45b687d33d1022f6f20a142bb27
fariya-nika-oshru <fariyanikaoshru@gmail.com>
2025-09-27 16:14:53 +0600

    fetched countries

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
+import { Suspense } from 'react'
 import './App.css'
 import Countries from './components/Countries/Countries'
 
 const countriesPromise = fetch('https://openapi.programming-hero.com/api/all')
   .then(res => res.json())
+
 function App() {
-
-
   return (
     <>
-      <h1>React World on the go....</h1>
-      <Suspense fallback ={<p>Nadir on the go loading.....</p>}>
+      <Suspense fallback = { <p>Nadir on the go loading.....</p> }>
         <Countries countriesPromise = {countriesPromise}></Countries>
       </Suspense>
       
--- src/component/Countries/Countries.jsx
+++ src/component/Countries/Countries.jsx
@@ -1,14 +1,14 @@
 import React, { use } from 'react';
 
 const Countries = ({countriesPromise}) => {
-    
-    const countries = use(countriesPromise);
+
+    const countriesData = use(countriesPromise);
+    const countries =countriesData.countries;
     console.log(countries);
 
     return (
         <div>
-            <h1>In the countries</h1>
-            
+            <h1>In the countries : {countries.length} </h1>
         </div>
     );
 };
